845d49e19f16c4de1432ddb100cdab6691e580a1
Alya <alyaramadhani000@gamil.com>
2021-03-11 18:38:40 +0700

    Soal_1

--- Soal_1.py
+++ Soal_1.py
@@ -1,1 +1,15 @@
 #menghitung luas persegi panjang
+
+p = float(input("Masukkan panjang: "))
+l = float(input("Masukkan lebar: "))
+
+luas_persegi_panjang = p * l
+print("luas persegi panjang:", luas_persegi_panjang)
+
+#menghitung luas lingkaran
+
+r = float(input("Masukkan jari-jari: "))
+phi = 3.14
+
+luas_lingkaran = 3.14 * (r ** 2)
+print("luas lingkaran:", luas_lingkaran)

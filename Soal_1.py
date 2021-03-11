e2437db171886c06bbaa10ecbf64ccce6856db28
Alya <alyaramadhani000@gamil.com>
2021-03-11 19:42:38 +0700

    Exercise 2.2

--- Soal_1.py
+++ Soal_1.py
@@ -1,5 +1,4 @@
 #menghitung luas persegi panjang
-
 p = float(input("Masukkan panjang: "))
 l = float(input("Masukkan lebar: "))
 
@@ -7,9 +6,32 @@ luas_persegi_panjang = p * l
 print("luas persegi panjang:", luas_persegi_panjang)
 
 #menghitung luas lingkaran
-
 r = float(input("Masukkan jari-jari: "))
 phi = 3.14
 
 luas_lingkaran = 3.14 * (r ** 2)
 print("luas lingkaran:", luas_lingkaran)
+
+#menghitung luas kubus
+r = float(input("Masukkan rusuk: "))
+
+luas_kubus = 6 * r * r
+print("luas kubus:", luas_kubus)
+
+#menghitung nilai suhu Celcius ke Fahrenheit
+print("Konversi suhu (Celcius ke Fahrenheit)")
+
+C = float(input("Masukkan suhu (Celcius): "))
+
+F = (9/5 * C) + 32
+print ("Celcius: ", C)
+print("Fahrenheit: ", F)
+
+#menghitung nilai suhu Reamur ke Kelvin
+print("Konversi suhu (Reamur ke Kelvin)")
+
+R = float(input("Masukkan suhu (Reamur): "))
+
+K = (5/4 * R) + 273
+print ("Reamur: ", R)
+print("Kelvin: ", K)

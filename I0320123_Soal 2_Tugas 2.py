d3a47d505b9014dcf10e57a7e843cefc8c9a33b8
Alya <alyaramadhani000@gamil.com>
2021-03-11 22:31:54 +0700

    Identitas diri

---
+++ I0320123_Soal 2_Tugas 2.py
@@ -0,0 +1,38 @@
+# Identitas Diri
+
+Nama_lengkap = "Alya Ramadhani"
+Nama_panggilan = "Alya"
+Umur = 19
+Anak_ke = 2
+Saudara = "perempuan"
+Kakak = "Amalia NK"
+Adik = "Alyssa Taqiyyah"
+Golongan_darah = 'O'
+Alamat = "Jalan Garuda 3 Tanon Lor RT02 RW02 Gedongan, Colomadu, Karanganyar, Jawa Tengah"
+Pekerjaan = "Mahasiswa"
+Tempat_kuliah = "Universitas Sebelas Maret"
+Fakultas = "Teknik"
+Jurusan = "Teknik Industri"
+Angkatan = 2020
+Motto_hidup = "Take It or Leave It"
+Hobi = "mendengarkan musik dan jalan-jalan"
+Tinggi_badan = 158
+Berat_badan = 46.5
+Makanan_favorit = "nasi goreng"
+Minuman_favorit = "susu"
+Warna_favorit = "putih dan kuning"
+Ukuran_sepatu = 39
+
+
+print("Hallo perkenalkan nama saya ", Nama_lengkap)
+print("Saya biasa dipanggil ", Nama_panggilan)
+print("Saat ini saya berumur ", Umur, "tahun")
+print("Saya anak ke ", 2, "dari 3 bersaudara")
+print("Saya memiliki 2 saudara ", Saudara, "yaitu kakak saya ", Kakak, "dan adik saya ", Adik)
+print("Golongan darah saya adalah ", Golongan_darah)
+print("Saat ini saya tinggal di ", Alamat, "bersama dengan keluarga saya")
+print("Saya merupakan seorang ", Pekerjaan, "di ", Tempat_kuliah, "pada fakultas ", Fakultas, "jurusan ", Jurusan, "angkatan ", Angkatan)
+print("Motto hidup saya yaitu ", Motto_hidup)
+print("Saya sangat suka ", Hobi)
+print("Tinggi badan saya sekarang adalah ", Tinggi_badan, "cm", "dan berat badan saya ", Berat_badan, "kilogram ")
+print("Saya sangat suka dengan ", Makanan_favorit, "dan ", Minuman_favorit, "karena menurut saya keduanya memiliki rasa yang sangat enak")
